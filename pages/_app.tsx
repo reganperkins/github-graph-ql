67ebdddaac14088efbea18b565ac9d5126bfca19
regan little <reganllittle@gmail.com>
2022-06-29 14:16:53 -0700

    add inital files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import axios from 'axios';
+
+const axiosGitHubGraphQL = axios.create({
+  baseURL: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: `bearer ${
+      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    }`,
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
